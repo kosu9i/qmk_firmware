bdb1ee6cdc72fba185fd9bc4d7b62473ee8d3b61
tomoki.kosugi <tomoki.kosugi@brainpad.co.jp>
2020-01-13 13:45:24 +0900

    KC_MUTE, KC_PDOTをfnレイヤに追加

--- keyboards/playkbtw/pk60/keymaps/custom/keymap.c
+++ keyboards/playkbtw/pk60/keymaps/custom/keymap.c
@@ -17,8 +17,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RESET,   KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
         _______, RGB_TOG,  RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, _______, KC_7,    KC_8,    KC_9,    _______, _______, _______, _______,
         _______, RGB_MOD,  _______, _______, RGB_VAI, RGB_VAD, _______, KC_4,    KC_5,    KC_6,    _______, _______, _______,
-        _______, RGB_RMOD, _______, BL_DEC,  BL_TOGG, BL_INC,  _______, KC_1,    KC_2,    KC_3,    _______, _______, KC_VOLU, _______,
-        _______, _______,  _______, _______,                            _______, KC_0, _______, _______, KC_SLCK, KC_VOLD, KC_PAUS 
+        _______, RGB_RMOD, _______, BL_DEC,  BL_TOGG, BL_INC,  _______, KC_1,    KC_2,    KC_3,    _______, KC_MUTE, KC_VOLU, _______,
+        _______, _______,  _______, _______,                            _______, KC_0, KC_PDOT,    _______, KC_SLCK, KC_VOLD, KC_PAUS 
     ),
 
     [2] = LAYOUT_minila(
