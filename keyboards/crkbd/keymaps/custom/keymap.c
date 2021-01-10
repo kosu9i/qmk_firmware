614accbea3667668bb664a0050bd64a1eff67e5f
tomoki.kosugi <student-00-36dab8db64a2@qwiklabs.net>
2021-01-10 13:19:59 +0900

    vold, voluに対応

--- keyboards/crkbd/keymaps/custom/keymap.c
+++ keyboards/crkbd/keymaps/custom/keymap.c
@@ -85,9 +85,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R,RGB_M_SW,RGB_M_SN,                      RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, EEP_RST,   RESET,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,                      KC_BSPC, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX, 
+      KC_LCTL, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,                      KC_BSPC, KC_VOLD, KC_VOLU, KC_MUTE, XXXXXXX, XXXXXXX, 
   //|--------+--------+--------+--------+--------+-                            |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD,                      KC_MUTE, KC_SLCK, KC_PAUS, XXXXXXX, XXXXXXX,  SECRET,
+      KC_LSFT,RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD,                      XXXXXXX, KC_SLCK, KC_PAUS, XXXXXXX, XXXXXXX,  SECRET,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
